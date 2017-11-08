c28545a77b1f3ef4efef16a807a0dfb2e7b695f6
dmitry.m <dmitry.m@sbtech.com>
2017-11-08 17:42:21 +0200

    winner definition logic fix.

--- ClientApp/app/domain/game.ts
+++ ClientApp/app/domain/game.ts
@@ -15,7 +15,7 @@ export class Game {
 
     onGameOver: (result: string) => void
 
-    tryMakeStep(r: number, c: number) {
+    tryMakeStep(r: number, c: number) { 
         if (!this.stepIsAllowed(r, c)) {
             return false;
         }
@@ -42,17 +42,22 @@ export class Game {
     }
 
     checkIsOver(bySteps: Array<GameStep>) {
+        // check for draw case
+        if (bySteps.length == 5) {
+            this.result = GameResult[GameResult.Draw];
+        }
+
         //checking for diagonal cases
-        if (bySteps.filter(s => s.vector % 2 == 0).length == 3) {
+        let distinctByVectorSteps = this.getDistinctByVector(bySteps);
+        let diagSign1 = distinctByVectorSteps.length === 1;
+        let diagSign2 = distinctByVectorSteps.length === 3;
+
+        if (bySteps.filter(s => s.vector % 2 == 0).length == 3
+            && (diagSign1 || diagSign2)) {
             this.result = GameResult[this.stepBy];
         }
 
         for (let i = 1; i <= Game.sheetSize; i++) {
-            // check for draw case
-            if (bySteps.length >= 4) {
-                this.result = GameResult[GameResult.Draw];
-            }
-
             // check for horisontal case
             if (bySteps.filter(s => s.c == i).length == 3) {
                 this.result = GameResult[this.stepBy];
@@ -74,5 +79,11 @@ export class Game {
         stepInto.push(new GameStep(r, c));
 
         return stepInto;
+    }
+
+    getDistinctByVector(inArray: Array<GameStep>) {
+        return inArray.filter(function (item, index) {
+            return inArray.findIndex(i => i.vector === item.vector) == index;
+        });
     }
 }
