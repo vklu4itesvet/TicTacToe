f59a9feb14d641281aef1e32f7f40150fb7f4600
dmitry.m <dmitry.m@sbtech.com>
2017-11-08 16:05:33 +0200

    small refo of step hanling sequence.

--- ClientApp/app/domain/game.ts
+++ ClientApp/app/domain/game.ts
@@ -20,16 +20,8 @@ export class Game {
             return false;
         }
 
-        this.state = GameState.InPlay;
-
-        let stepInto = this.stepBy == PlayerType.X ? this.xSteps : this.oSteps;
-        stepInto.push(new GameStep(r, c));
-
-        let res = this.checkIsOver(stepInto);
-        if (res != null) {
-            this.state = GameState.Finished;
-            this.result = res;
-            this.onGameOver(res);
+        if (this.checkIsOver(this.saveStep(r, c))) {
+            this.onGameOver(this.result ? this.result : '');
         }
         else {
             this.stepBy = this.stepBy * (-1);//switching current player
@@ -52,26 +44,35 @@ export class Game {
     checkIsOver(bySteps: Array<GameStep>) {
         //checking for diagonal cases
         if (bySteps.filter(s => s.vector % 2 == 0).length == 3) {
-            return GameResult[this.stepBy];
+            this.result = GameResult[this.stepBy];
         }
 
         for (let i = 1; i <= Game.sheetSize; i++) {
             // check for draw case
             if (bySteps.length >= 4) {
-                return GameResult[GameResult.Draw];
+                this.result = GameResult[GameResult.Draw];
             }
 
             // check for horisontal case
             if (bySteps.filter(s => s.c == i).length == 3) {
-                return GameResult[this.stepBy];
+                this.result = GameResult[this.stepBy];
             }
 
             // check for vertical case
             if (bySteps.filter(s => s.r == i).length == 3) {
-                return GameResult[this.stepBy];
+                this.result = GameResult[this.stepBy];
             }
         }
 
-        return null;
+        this.state = this.result === undefined ? GameState.InPlay : GameState.Finished;
+
+        return this.state == GameState.Finished;
+    }
+
+    saveStep(r: number, c: number) {
+        let stepInto = this.stepBy == PlayerType.X ? this.xSteps : this.oSteps;
+        stepInto.push(new GameStep(r, c));
+
+        return stepInto;
     }
 }
